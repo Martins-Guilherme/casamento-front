81edceebf3a05363b64b531fd37eb20d6bee4377
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-10-08 01:51:20 -0300

    feat: implement admin dashboard with data fetching and visualization components

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,81 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import { Gift, UserCheckIcon } from "lucide-react";
+import Infocard from "@/app/_components/Infocard";
+import BarChart from "@/app/_components/BarChart";
+
+interface DashboardData {
+  totalPresentesSelecionados: number;
+  totalConvidadosConfirmados: number;
+  totalPresentesDisponiveis: number;
+  presentesPopulares: { nome: string; count: number[] };
+}
+
 const AdminPage = () => {
+  const [data, setData] = useState<DashboardData | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetcheDashboardData() {
+      const res = await fetch("/api/admin/dashboard");
+      if (res.ok) {
+        const json = await res.json();
+        setData(json);
+      }
+      setLoading(false);
+    }
+    fetcheDashboardData();
+  }, []);
+
+  if (loading)
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <p className="bg-white p-8 flex justify-center items-center text-red-300 uppercase text-shadow-2xs rounded-2xl shadow text-3xl font-bold mb-6">
+          Carregando...
+        </p>
+      </div>
+    );
+  if (!data)
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <p className="bg-white p-8 flex justify-center items-center text-red-400 uppercase text-shadow-2xs rounded-2xl shadow text-3xl font-bold mb-6">
+          Erro ao carregar os dados!
+        </p>
+      </div>
+    );
+
+  const labels = ["Disponíveis", "Escolhidos"];
+  const counts = [
+    data.totalPresentesDisponiveis,
+    data.totalPresentesSelecionados,
+  ];
+
   return (
-    <div className="flex justify-center">
-      <h1 className="text-red-300">Dashboard em construção</h1>
+    <div className="max-w-dvh mx-auto text-center flex flex-col items-center mb-8">
+      <h1 className="bg-white p-8 text-red-300 uppercase text-shadow-2xs rounded-2xl shadow text-3xl font-bold mb-6">
+        Dashboard
+      </h1>
+      <div className=" bg-white rounded-lg shadow p-3 overflow-hidden">
+        <div className="p-6 space-y-8 flex w-fit justify-center max-w-lg items-center">
+          <div className="grid grid-cols-1 gap-24">
+            <Infocard
+              title="Convidados Confirmados"
+              value={data.totalConvidadosConfirmados}
+              icon={<UserCheckIcon size={32} />}
+            />
+            <Infocard
+              title="Presentes Cadastrados"
+              value={data.totalPresentesDisponiveis}
+              icon={<Gift size={32} />}
+            />
+            <div className="flex w-full">
+              <BarChart labels={labels} data={counts} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/app/api/admin/dashboard/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/app/_lib/prisma";
+
+export async function GET() {
+  try {
+    // Total de convidados que já selecionaram os presentes
+    const totalConvidadosConfirmados = await prisma.convidado.count({
+      where: { usado: true },
+    });
+
+    const totalPresentesSelecionados = await prisma.convidado.count({
+      where: { presenteId: { not: null } },
+    });
+
+    const totalPresentesDisponiveis = await prisma.tabelaDePresentes.count();
+
+    const presentesPopulares = await prisma.convidado.groupBy({
+      by: ["presenteId"],
+      _count: { presenteId: true },
+      orderBy: { _count: { presenteId: "desc" } },
+      take: 5,
+    });
+
+    return NextResponse.json({
+      totalConvidadosConfirmados,
+      totalPresentesSelecionados,
+      totalPresentesDisponiveis,
+      presentesPopulares,
+    });
+  } catch (erro) {
+    console.error(erro);
+    return NextResponse.json(
+      { erro: "Erro ao calcular e carregar os dados" },
+      { status: 500 }
+    );
+  }
+}
